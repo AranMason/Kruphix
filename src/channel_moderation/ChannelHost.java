f52e50ac2caf80eaafb2fa2b9d14f46dfdf7047b
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-09-25 19:19:20 +1300

    Changed the handling of commands for channel creation

--- src/channel_moderation/ChannelHost.java
+++ src/channel_moderation/ChannelHost.java
@@ -2,53 +2,37 @@ package channel_moderation;
 
 import java.util.Timer;
 
-import net.dv8tion.jda.entities.VoiceChannel;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.managers.ChannelManager;
 
 public class ChannelHost {
 
-	private final static String CHANNEL_CREATE_COMMAND = "!server";
-	private final static String WIPE_POPUP_CHANNELS = "!wipe";
-		
-	public ChannelHost(){
-		
-	}
+	//The command to invoke the creation of a new voice channel.
+	private final static String CHANNEL_CREATE_COMMAND = "!channel";
+	//The default name for any new channel, when a name is not given.
+	private final static String DEFAULT_CHANNEL_NAME = "TempChannel";
 
 	public String getChannelCreateCommand() {
 		return CHANNEL_CREATE_COMMAND;
 	}
-	
-	public String getWipeChannelsCommand(){
-		return WIPE_POPUP_CHANNELS;
-	}
 
 	public void createChannel(MessageReceivedEvent event) {
 		
-		System.out.println("Creating Channel");
+		String content = event.getMessage().getContent();
+		//Get the name of the channel, otherwise use a default name.
+		String channel_name = 
+				(content.length() > CHANNEL_CREATE_COMMAND.length()+1) ?
+						content.substring(CHANNEL_CREATE_COMMAND.length()+1, content.length()) :
+							DEFAULT_CHANNEL_NAME;
+		//If the channels name exceeds the character limit just crop it to the character limit.				
+		if(channel_name.length() > 100)
+			channel_name = channel_name.substring(0, 100);
 		
-		String[] args = event.getMessage().getContent().split("\\s+");
+		//Create the channel.
+		ChannelManager channel = event.getGuild().createVoiceChannel(channel_name);
 		
-		System.out.println("~" + args.length);
-		//If we don't have a name we can create a channel.
-		if(args.length <= 1){
-			event.getChannel().sendMessage("Every new world has a name.");
-			return;
-		}
-		
-		for(VoiceChannel v : event.getGuild().getVoiceChannels()){
-			if(v.getName().equals(args[1]) || args[1].equals("")){
-				event.getChannel().sendMessage("Please be more original");
-				return;
-			}
-		};
-		
-		ChannelManager channel = event.getGuild().createVoiceChannel(args[1]);
-		System.out.println(channel);
-		
-		//Default set expiry time to be in 12 hours.
+		//Set up a delayed event for the channel to self delete.
 		Timer t = new Timer();
-		
 		t.schedule(new ChannelHandler(channel), ChannelHandler.CHANNEL_LIFE_SPAN);		
 	}
 }
