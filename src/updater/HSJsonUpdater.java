23ab82914f2e658449de7879e01bbbda59b49413
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-10-25 09:29:46 +1300

    HSchanges

---
+++ src/updater/HSJsonUpdater.java
@@ -0,0 +1,26 @@
+package updater;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class HSJsonUpdater implements Updater{
+
+	@Override
+	public boolean checkForUpdates() throws MalformedURLException, IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object loadJSONFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
